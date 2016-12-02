1d41b33a9a1e7b8d1be7d1a3b9a191840b236681
AlexaPyth <shakam77@gmail.com>
2016-12-02 10:01:49 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,10 +1,10 @@
-mkdir -p ~/web/{public/{img,js,css},{etc,uploads}}
+#mkdir -p ~/web/{public/{img,js,css},{etc,uploads}}
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 
 sudo /etc/init.d/nginx restart
 
 sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
-#sudo /etc/init.d/gunicorn restart
-#﻿sudo /etc/init.d/mysql start
+sudo /etc/init.d/gunicorn restart
+#sudo /etc/init.d/mysql start
 
 #sudo /etc/init.d/gunicorn restart 
