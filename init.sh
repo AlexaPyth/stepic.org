d19d744ad1058083cfd1e1880f8383258049c2eb
AlexaPyth <shakam77@gmail.com>
2016-12-08 16:53:28 +0200

    Greate Django App, gunicorn for django,nginx -proxy 8000

--- init.sh
+++ init.sh
@@ -1,9 +1,12 @@
 #mkdir -p ~/web/{public/{img,js,css},{etc,uploads}}
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 
-sudo /etc/init.d/nginx estart
+sudo /etc/init.d/nginx start
 
 sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
+
+sudo ln -sf /home/box/web/etc/gunicorn-django.conf /etc/gunicorn.d/test-django
+
 sudo /etc/init.d/gunicorn start
 #sudo /etc/init.d/mysql start
 
---
+++ web/ask/qa/urls.py
@@ -0,0 +1,27 @@
+"""{{ app_name }} URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/{{ docs_version }}/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url
+from .views import test
+
+urlpatterns = [
+    url(r'^$', test, name='index'),
+    url(r'^login/', test, name='login'),
+    url(r'^signup/', test, name='signup'),
+    url(r'^ask/', test, name='ask'),
+    url(r'^popular/', test, name='popular'),
+    url(r'^new/', test, name='new'),
+    url(r'^question/[<](?P<pk>\d+)[>]/$', test, name='question'),
+]
